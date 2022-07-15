98f2adf9b936ec4025580f89975d78f72dee5ea3
anx450z <anx450z@gmail.com>
2022-07-16 01:21:16 +0530

    start with Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import FavoritesPage from "./pages/Favorites";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupsPage from "./pages/NewMeetups";
+
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetups" element={<NewMeetupsPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
     </div>
   );
 }
