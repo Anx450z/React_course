af6873077e6ba3ae4d31176aca01a854b6be3c5f
anx450z <anx450z@gmail.com>
2022-07-16 00:57:16 +0530

    New Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import Todo from "./components/Todo";
-
 function App() {
   return (
     <div>
-      <h1>My Todo</h1>
-      <Todo text='Learn React' />
-      <Todo text='Make login using React' />
-      <Todo text='Node js' />
     </div>
   );
 }
