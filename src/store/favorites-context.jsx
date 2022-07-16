7c9c45ba9765cc19af237d70ba5dd31c39cec69c
anx450z <anx450z@gmail.com>
2022-07-16 18:24:58 +0530

    contextand different ways of updating states

--- src/store/favorites-context.jsx
+++ src/store/favorites-context.jsx
@@ -8,6 +8,22 @@ const FavoritesContext = createContext({
 function FavoritesContextProvider(props) {
   const [userFavorites, setUserFavorites] = useState([]);
 
+  function addFavorite(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  }
+
+  function removeFavorite(meetupId) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+
+  function itemIsFavorite(meetupId) {
+    return userFavorites.some(meetup => meetup.id === meetupId);
+  }
+
   const context = {
     favorites: userFavorites,
     totalFavorites: userFavorites.length,
