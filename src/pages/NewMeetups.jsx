adf9f8cf8474397754af78b48427925475e65ee8
anx450z <anx450z@gmail.com>
2022-07-16 12:36:11 +0530

    add new meetup page using a form

--- src/pages/NewMeetups.jsx
+++ src/pages/NewMeetups.jsx
@@ -1,5 +1,10 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupsPage() {
-  return <div>New meetup page</div>;
+  return <section>
+    <h1>Add new Meetup</h1>
+    <NewMeetupForm/>
+  </section>
 }
 
 export default NewMeetupsPage;
