3a931b8cf09a3ce09bc29b99f0bddc66ab93797a
anx450z <anx450z@gmail.com>
2022-07-16 13:20:21 +0530

    send post request using fetch

--- src/pages/NewMeetups.jsx
+++ src/pages/NewMeetups.jsx
@@ -1,14 +1,26 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
-  function addMeetup(meetupData){
-    
+  function addMeetup(meetupData) {
+    //use to send HTTP request
+    fetch(
+      "https://react-course-95a67-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: 'POST', // POST method instead of GET
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ); 
   }
 
-  return <section>
-    <h1>Add new Meetup</h1>
-    <NewMeetupForm onAddMeetup={addMeetup} />
-  </section>
+  return (
+    <section>
+      <h1>Add new Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetup} />
+    </section>
+  );
 }
 
 export default NewMeetupsPage;
